472e54717bdd6f2a3fa710c8641b1a3fa9d92fff
ny-a <58758248+ny-a@users.noreply.github.com>
2021-06-09 23:00:23 +0900

    Refactor sin/cos argument

--- rtty8k.py
+++ rtty8k.py
@@ -15,10 +15,11 @@ def read_wave_file(filename, mark_frequency = 914, space_frequency = 1086):
         for j in range(wave_file.getnframes()):
             buf = wave_file.readframes(1)
             array_index = j % window_size
-            mark_q[array_index] = int((buf[0]-128) * np.sin(np.pi * 2.0 / sampling_rate * mark_frequency * j))
-            mark_i[array_index] = int((buf[0]-128) * np.cos(np.pi * 2.0 / sampling_rate * mark_frequency * j))
-            space_q[array_index] = int((buf[0]-128) * np.sin(np.pi * 2.0 / sampling_rate * space_frequency * j))
-            space_i[array_index] = int((buf[0]-128) * np.cos(np.pi * 2.0 / sampling_rate * space_frequency * j))
+            factor = np.pi * 2.0 * j / sampling_rate
+            mark_q[array_index] = int((buf[0]-128) * np.sin(factor * mark_frequency))
+            mark_i[array_index] = int((buf[0]-128) * np.cos(factor * mark_frequency))
+            space_q[array_index] = int((buf[0]-128) * np.sin(factor * space_frequency))
+            space_i[array_index] = int((buf[0]-128) * np.cos(factor * space_frequency))
             mark_value = np.sqrt(sum(mark_q)**2 + sum(mark_i)**2)
             space_value = np.sqrt(sum(space_q)**2 + sum(space_i)**2)
             print(mark_value, space_value, int(mark_value > space_value), sep=",")

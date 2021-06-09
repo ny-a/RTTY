147f24551a68de2506c7639576437bf862f37b18
ny-a <58758248+ny-a@users.noreply.github.com>
2021-06-09 22:44:16 +0900

    Use array for ring buffer instead of list

--- rtty8k.py
+++ rtty8k.py
@@ -1,5 +1,6 @@
 import wave
 import numpy as np
+import array
 
 
 filename = 'rtty3s.wav'  # should be specify the filename.
@@ -10,21 +11,17 @@ window_size = 32           # windows size Integer
 
 
 with wave.open(filename, 'rb') as wave_file:
-    mark_q = []
-    mark_i = []
-    space_q = []
-    space_i = []
+    mark_q = array.array('l', [0] * window_size)
+    mark_i = array.array('l', [0] * window_size)
+    space_q = array.array('l', [0] * window_size)
+    space_i = array.array('l', [0] * window_size)
     for j in range(wave_file.getnframes()):
         buf = wave_file.readframes(1)
-        mark_q.append((buf[0]-128)*np.sin(np.pi*2.0/mark_frequency*j))
-        mark_i.append((buf[0]-128)*np.cos(np.pi*2.0/mark_frequency*j))
-        space_q.append((buf[0]-128)*np.sin(np.pi*2.0/space_frequency*j))
-        space_i.append((buf[0]-128)*np.cos(np.pi*2.0/space_frequency*j))
+        array_index = j % window_size
+        mark_q[array_index] = int((buf[0]-128) * np.sin(np.pi * 2.0 / mark_frequency * j))
+        mark_i[array_index] = int((buf[0]-128) * np.cos(np.pi * 2.0 / mark_frequency * j))
+        space_q[array_index] = int((buf[0]-128) * np.sin(np.pi * 2.0 / space_frequency * j))
+        space_i[array_index] = int((buf[0]-128) * np.cos(np.pi * 2.0 / space_frequency * j))
         mark_value = np.sqrt(sum(mark_q)**2 + sum(mark_i)**2)
         space_value = np.sqrt(sum(space_q)**2 + sum(space_i)**2)
         print(mark_value, space_value, int(mark_value > space_value), sep=",")
-        if j > window_size:
-            mark_q.pop(0)
-            mark_i.pop(0)
-            space_q.pop(0)
-            space_i.pop(0)

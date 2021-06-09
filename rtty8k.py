cc16b62322ebc575b7540a1eee968d394e7e4730
ny-a <58758248+ny-a@users.noreply.github.com>
2021-06-09 22:39:44 +0900

    Refactor variable name

--- rtty8k.py
+++ rtty8k.py
@@ -2,29 +2,29 @@ import wave
 import numpy as np
 
 
-fname = 'rtty3s.wav'  # should be specify the filename.
-smp = 8000          # Sampling Rate
-FQm = smp/914.0     # Mark Frequency 914Hz
-FQs = smp/1086.0    # Space Frequency 1086Hz
-wind = 32           # windows size Integer
+filename = 'rtty3s.wav'  # should be specify the filename.
+sampling_rate = 8000          # Sampling Rate
+mark_frequency = sampling_rate / 914.0     # Mark Frequency 914Hz
+space_frequency = sampling_rate / 1086.0    # Space Frequency 1086Hz
+window_size = 32           # windows size Integer
 
 
-with wave.open(fname, 'rb') as waveFile:
-    mq = []
-    mi = []
-    sq = []
-    si = []
-    for j in range(waveFile.getnframes()):
-        buf = waveFile.readframes(1)
-        mq.append((buf[0]-128)*np.sin(np.pi*2.0/FQm*j))
-        mi.append((buf[0]-128)*np.cos(np.pi*2.0/FQm*j))
-        sq.append((buf[0]-128)*np.sin(np.pi*2.0/FQs*j))
-        si.append((buf[0]-128)*np.cos(np.pi*2.0/FQs*j))
-        mk = np.sqrt(sum(mq)**2 + sum(mi)**2)
-        sp = np.sqrt(sum(sq)**2 + sum(si)**2)
-        print(mk, sp, int(mk > sp), sep=",")
-        if j > wind:
-            mq.pop(0)
-            mi.pop(0)
-            sq.pop(0)
-            si.pop(0)
+with wave.open(filename, 'rb') as wave_file:
+    mark_q = []
+    mark_i = []
+    space_q = []
+    space_i = []
+    for j in range(wave_file.getnframes()):
+        buf = wave_file.readframes(1)
+        mark_q.append((buf[0]-128)*np.sin(np.pi*2.0/mark_frequency*j))
+        mark_i.append((buf[0]-128)*np.cos(np.pi*2.0/mark_frequency*j))
+        space_q.append((buf[0]-128)*np.sin(np.pi*2.0/space_frequency*j))
+        space_i.append((buf[0]-128)*np.cos(np.pi*2.0/space_frequency*j))
+        mark_value = np.sqrt(sum(mark_q)**2 + sum(mark_i)**2)
+        space_value = np.sqrt(sum(space_q)**2 + sum(space_i)**2)
+        print(mark_value, space_value, int(mark_value > space_value), sep=",")
+        if j > window_size:
+            mark_q.pop(0)
+            mark_i.pop(0)
+            space_q.pop(0)
+            space_i.pop(0)

3a10705e33757a90bd69a4a1d5d6942628ede657
ny-a <58758248+ny-a@users.noreply.github.com>
2021-06-24 11:42:31 +0900

    Implement ITA2 parser

--- src/parser.py
+++ src/parser.py
@@ -2,6 +2,7 @@ import sys
 
 from .read_wave_file import read_wave_file
 from .frame_filter import frame_to_bit_chunks
+from .parse_ita2 import parse_ita2
 
 
 if __name__ == '__main__':
@@ -13,5 +14,7 @@ if __name__ == '__main__':
     with open('{}.csv'.format(filename), 'w') as output:
         frame_values = read_wave_file(filename, signed=False)
         bit_chunks = frame_to_bit_chunks(frame_values, baud_rate=45.45, start_bit=0, stop_bit=1)
-        for bit_chunk in bit_chunks:
-            print(bit_chunk)
+        ita2_chars = parse_ita2(bit_chunks)
+        for char, _ in ita2_chars:
+            print(char, end='', flush=True)
+        print('')

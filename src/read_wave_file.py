6d33ae70c90ca8c2ad130a730735c0ba685e4484
ny-a <58758248+ny-a@users.noreply.github.com>
2021-08-11 01:04:32 +0900

    Replace numpy with math

--- src/read_wave_file.py
+++ src/read_wave_file.py
@@ -1,7 +1,7 @@
 import wave
-import numpy as np
+import math
 
-from .window_sum import WindowSum 
+from .window_sum import WindowSum
 
 def read_wave_file(filename, signed = False, mark_frequency = 914, space_frequency = 1086):
     byteorder = 'little'
@@ -22,11 +22,11 @@ def read_wave_file(filename, signed = False, mark_frequency = 914, space_frequen
         for j in range(wave_file.getnframes()):
             frame_value = int.from_bytes(wave_file.readframes(1), byteorder=byteorder, signed=signed) - offset
             time = j / sampling_rate
-            factor = np.pi * 2.0 * time
-            mark_q.set(int(frame_value * np.sin(factor * mark_frequency)))
-            mark_i.set(int(frame_value * np.cos(factor * mark_frequency)))
-            space_q.set(int(frame_value * np.sin(factor * space_frequency)))
-            space_i.set(int(frame_value * np.cos(factor * space_frequency)))
+            factor = math.pi * 2.0 * time
+            mark_q.set(int(frame_value * math.sin(factor * mark_frequency)))
+            mark_i.set(int(frame_value * math.cos(factor * mark_frequency)))
+            space_q.set(int(frame_value * math.sin(factor * space_frequency)))
+            space_i.set(int(frame_value * math.cos(factor * space_frequency)))
             mark_value = mark_q.sum * mark_q.sum + mark_i.sum * mark_i.sum
             space_value = space_q.sum * space_q.sum + space_i.sum * space_i.sum
             yield (mark_value, space_value, time)
